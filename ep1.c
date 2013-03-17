493b73422670da7c6594a17deb72bd384ba9388e
Ruan Costa <ruan.cocito@gmail.com>
2013-03-17 12:42:25 -0300

    Implementando opcoes

--- ep1.c
+++ ep1.c
@@ -1,103 +1,109 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include "Ponto.h"
 
-typedef struct ponto{float x;
-                       float y;
-                       struct ponto *ant;
-                       struct ponto *prox;}Ponto;
-
-Ponto *geraPontos(int);
-float calculaDistancia(Ponto, Ponto);
-void *mallocSafe (size_t);
+#define FALSE 0
+#define TRUE 1
+#define DEFAULT 13
 
 int main(int argc, char* argv[])
 {/*Procurar o ponto P mais proximo do ultimo elemento do grafo, ver se tem algum outro ponto do grafo
    que eh mais proximo de P, e se tiver, ligar P com ele*/
-   int N;
-   float candMenorDist, menor, maiorMenores;
+
+   int N,M,v,V,s, cont;
+   float candMenorDist, menor, maiorMenores, d, somatoriaQuadrados;
    Ponto *cabPontos, *cabGrafo, *novoPontoGrafo, *i, *j, *k,*aux;
    cabGrafo = mallocSafe(sizeof(*cabPontos));
 
-   N = atoi(argv[1]);
-   cabPontos = geraPontos(N);
+   M=1; d=-1; v=FALSE; V=FALSE; s=DEFAULT;
 
-   menor = 1000;
-   maiorMenores = -1;
+   for(i=0; i<argc; i++)
+   {
+      if(argv[i][0] == '-' && argv[i][1] == 'N')
+      {
+         N = atoi(&argv[i][2]);
+      }
+      if(argv[i][0] == '-' && argv[i][1] == 'M')
+      {
+         M = atoi(argv[i]+2);
+      }
+      if(argv[i][0] == '-' && argv[i][1] == 'd')
+      {
+         d = atof(argv[i]+2);
+      }
+      if(argv[i][0] == '-' && argv[i][1] == 'v')
+      {
+         v = TRUE;
+      }
+      if(argv[i][0] == '-' && argv[i][1] == 'V')
+      {
+         V = TRUE;
+      }
+      if(argv[i][0] == '-' && argv[i][1] == 's')
+      {
+         s = atoi(argv[i]+2);
+      }
+   }
+   srand(s);
+   for(cont = 0; cont < M; cont++)
+   {
+      cabPontos = geraPontos(N);
 
-   aux = cabPontos->prox;
-   cabPontos->prox = cabPontos->prox->prox;
-   cabPontos->prox->prox->ant = cabPontos;
+      menor = 1000;
+      maiorMenores = -1;
+      somatoriaQuadrados = 0;
 
-   aux->prox = NULL;
-   aux->ant = cabGrafo;
-   cabGrafo->prox = aux;
-   cabGrafo->ant = NULL;
+      if(N==1)maiorMenores = 0;
+      else
+      {
+         aux = cabPontos->prox;
+         cabPontos->prox = cabPontos->prox->prox;
+         aux->prox->ant = cabPontos;
+
+         aux->prox = NULL;
+         aux->ant = cabGrafo;
+         cabGrafo->prox = aux;
+         cabGrafo->ant = NULL;
 
 
-    for(i = cabGrafo->prox; i != NULL; i = i->prox)
-    {
-       for(j = cabPontos->prox; j != NULL; j = j->prox)
-       {
-          candMenorDist = calculaDistancia(*i,*j);
-          if(candMenorDist < menor)
+          for(i = cabGrafo->prox; i != NULL; i = i->prox)
           {
-            menor = candMenorDist;
-            novoPontoGrafo = j;
+             for(j = cabPontos->prox; j != NULL; j = j->prox)
+             {
+                candMenorDist = calculaDistancia(*i,*j);
+                if(candMenorDist < menor)
+                {
+                  menor = candMenorDist;
+                  novoPontoGrafo = j;
+                }
+             }
+             for(k = cabGrafo->prox; k != NULL; k = k->prox)
+             {
+                candMenorDist = calculaDistancia(*k,*novoPontoGrafo);
+                if(candMenorDist < menor)menor = candMenorDist;
+             }
+
+             if(novoPontoGrafo->prox != NULL)novoPontoGrafo->prox->ant = novoPontoGrafo->ant;
+             novoPontoGrafo->ant->prox = novoPontoGrafo->prox;
+
+             i->prox = novoPontoGrafo;
+             novoPontoGrafo->ant = i;
+             novoPontoGrafo->prox = NULL;
+
+             if(menor > maiorMenores)maiorMenores = menor;
           }
-       }
-       for(k = cabGrafo->prox; k != NULL; k = k->prox)
-       {
-          candMenorDist = calculaDistancia(*k,*novoPontoGrafo);
-          if(candMenorDist < menor)menor = candMenorDist;
-       }
-
-       if(novoPontoGrafo->prox != NULL)novoPontoGrafo->prox->ant = novoPontoGrafo->ant;
-       novoPontoGrafo->ant->prox = novoPontoGrafo->prox;
-
-       i->prox = novoPontoGrafo;
-       novoPontoGrafo->ant = i;
-       novoPontoGrafo->prox = NULL;
-
-       if(menor > maiorMenores)maiorMenores = menor;
-    }
-    printf("D = %f\n", maiorMenores);
-
+          somatoriaQuadrados += maiorMenores*maiorMenores;
+          printf("Densidade normalizada crítica = %f\n", somatoriaQuadrados*N/log(N));
+      }
+   }
    return 0;
 }
 
-Ponto *geraPontos(int N)
+void imprimePontos(Pontos *cab)
 {
-   Ponto *p;
-   Ponto *cabPontos = mallocSafe(sizeof(*cabPontos));
-   cabPontos->ant = NULL;
-   cabPontos->prox = NULL;
-   while(N > 0)
-   {
-      p = mallocSafe(sizeof(*p));
-      p->x = 1.0*rand()/RAND_MAX;
-      p->y = 1.0*rand()/RAND_MAX;
-      if(cabPontos->prox != NULL)cabPontos->prox->ant = p;
-      p->ant = cabPontos;
-      p->prox = cabPontos->prox;
-      cabPontos->prox = p;
-      N--;
-   }
-   return cabPontos;
+   Pontos *p;
+   for(p=cab->prox; p!=NULL ; p = p->prox)
+      printf("(%f, %f)\n", p->x, p->y);
 }
 
-float calculaDistancia(Ponto a, Ponto b)
-{
-   return sqrt((a.x*a.x - b.x*b.x)+(a.y*a.y - b.y*b.y));
-}
-
-void *mallocSafe (size_t n)
-{
-  void * pt;
-  pt = malloc(n);
-  if (pt == NULL) {
-    printf("ERRO na alocacao de memoria.\n\n");
-    exit(-1);
-  }
-  return pt;
-}
